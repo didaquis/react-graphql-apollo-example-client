2e0da0d17bbb8c8002c8bf7f699c8f70c6e2aef6
Dídac García <didaquis@gmail.com>
2019-02-26 23:52:59 +0100

    Added form to register new clients

--- src/components/NuevoCliente.js
+++ src/components/NuevoCliente.js
@@ -1,13 +1,131 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+
+import { NUEVO_CLIENTE } from '../gql/mutations/index';
+import { Mutation } from 'react-apollo';
 
 class NuevoCliente extends Component {
 	state = {
-
+		cliente: {
+			nombre: '',
+			apellido: '',
+			empresa: '',
+			email: '',
+			tipo: ''
+		}
 	};
 
 	render() {
 		return (
-			<h2 className="text-center">Nuevo Cliente</h2>
+			<Fragment>
+				<h2 className="text-center">Nuevo Cliente</h2>
+				<div className="row justify-content-center">
+					<Mutation mutation={NUEVO_CLIENTE}>
+
+						{
+							crearCliente => (
+								<form className="col-md-8 m-3"
+									onSubmit={ e => {
+										e.preventDefault();
+										const {nombre, apellido, empresa, email, tipo} = this.state.cliente;
+										const input = {
+											nombre,
+											apellido,
+											empresa,
+											email,
+											tipo
+										};
+
+										crearCliente({
+											variables: {input}
+										})
+									}}
+								>
+									<div className="form-row">
+										<div className="form-group col-md-6">
+											<label>Nombre</label>
+											<input
+												type="text"
+												className="form-control"
+												placeholder="Nombre"
+												onChange={ e => {
+													this.setState({ cliente: {
+														...this.state.cliente,
+														nombre: e.target.value
+													}})
+												}}
+											/>
+										</div>
+										<div className="form-group col-md-6">
+											<label>Apellido</label>
+											<input
+												type="text"
+												className="form-control"
+												placeholder="Apellido"
+												onChange={ e => {
+													this.setState({ cliente: {
+														...this.state.cliente,
+														apellido: e.target.value
+													}})
+												}}
+											/>
+										</div>
+									</div>
+									<div className="form-row">
+										<div className="form-group col-md-6">
+											<label>Empresa</label>
+											<input
+												type="text"
+												className="form-control"
+												placeholder="Empresa"
+												onChange={ e => {
+													this.setState({ cliente: {
+														...this.state.cliente,
+														empresa: e.target.value
+													}})
+												}}
+											/>
+										</div>
+										<div className="form-group col-md-6">
+											<label>Email</label>
+											<input
+												type="email"
+												className="form-control"
+												placeholder="Email"
+												onChange={ e => {
+													this.setState({ cliente: {
+														...this.state.cliente,
+														email: e.target.value
+													}})
+												}}
+											/>
+										</div>
+									</div>
+									<div className="form-row">
+										<div className="form-group col-md-6">
+											<label>Tipo Cliente</label>
+											<select
+												onChange={ e => {
+													this.setState({ cliente: {
+														...this.state.cliente,
+														tipo: e.target.value
+													}})
+												}}
+												className="form-control">
+												<option value="">Elegir...</option>
+												<option value="PREMIUM">PREMIUM</option>
+												<option value="BASICO">BÁSICO</option>
+											</select>
+										</div>
+									</div>
+									<button type="submit" className="btn btn-success float-right">Guardar Cambios</button>
+								</form>
+							)
+						}
+
+
+					</Mutation>
+				</div>
+			</Fragment>
 		);
 	};
 };
