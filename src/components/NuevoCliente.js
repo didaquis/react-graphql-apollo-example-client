2b2faf870d4cdb2a1632261e6a82d879199dbc0e
Dídac García <didaquis@gmail.com>
2019-02-27 00:11:26 +0100

    Added some basic form validations

--- src/components/NuevoCliente.js
+++ src/components/NuevoCliente.js
@@ -11,22 +11,37 @@ class NuevoCliente extends Component {
 			empresa: '',
 			email: '',
 			tipo: ''
-		}
+		},
+		error: false
 	};
 
 	render() {
+		const { error } = this.state;
+		let respuesta = (error) ? <p className="alert alert-danger p-3 text-center">Todos los campos son obligatorios</p> : '';
+
 		return (
 			<Fragment>
 				<h2 className="text-center">Nuevo Cliente</h2>
+
+				{respuesta}
+
 				<div className="row justify-content-center">
 					<Mutation mutation={NUEVO_CLIENTE}>
-
 						{
 							crearCliente => (
 								<form className="col-md-8 m-3"
 									onSubmit={ e => {
 										e.preventDefault();
 										const {nombre, apellido, empresa, email, tipo} = this.state.cliente;
+
+										// validaciones
+										if (nombre === '' || apellido === '' || empresa === '' || email === '' || tipo === '') {
+											this.setState({error: true});
+											return;
+										}
+
+										this.setState({error: false});
+
 										const input = {
 											nombre,
 											apellido,
@@ -47,6 +62,8 @@ class NuevoCliente extends Component {
 												type="text"
 												className="form-control"
 												placeholder="Nombre"
+												required
+												autoFocus
 												onChange={ e => {
 													this.setState({ cliente: {
 														...this.state.cliente,
@@ -117,7 +134,7 @@ class NuevoCliente extends Component {
 											</select>
 										</div>
 									</div>
-									<button type="submit" className="btn btn-success float-right">Guardar Cambios</button>
+									<button type="submit" className="btn btn-success float-right">Crear nuevo cliente</button>
 								</form>
 							)
 						}
