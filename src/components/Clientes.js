fa15050220523f59ffb0e4ad93510ce89e255af3
Dídac García <didaquis@gmail.com>
2019-03-02 23:25:10 +0100

    Feature: delete clients from database

--- src/components/Clientes.js
+++ src/components/Clientes.js
@@ -1,8 +1,9 @@
 import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 
-import { Query } from 'react-apollo';
+import { Query, Mutation } from 'react-apollo';
 import { CLIENTES_QUERY } from '../gql/queries/index';
+import { ELIMINAR_CLIENTE } from '../gql/mutations/index';
 
 
 const Contactos = () => {
@@ -21,6 +22,8 @@ const Contactos = () => {
 						<ul className="list-group mt-4">
 							{
 								data.getClientes.map((cliente) => {
+									const { id } = cliente;
+
 									return (<li key={cliente.id} className="list-group-item">
 										<div className="row justify-content-between align items center">
 											<div className="col-md-8 d-flex justify-content-between align items center">
@@ -29,6 +32,28 @@ const Contactos = () => {
 													{cliente.empresa} {cliente.email}
 											</div>
 											<div className="col-md-4 d-flex justify-content-end align-items-center">
+												<Mutation
+													mutation={ELIMINAR_CLIENTE}
+												>
+
+
+													{ eliminarCliente => (
+														<button
+															type="button"
+															className="btn btn-danger d-block d-md-inline-block mr-2"
+															onClick={ () => {
+																const message = '¿Seguro que quieres eliminar el cliente?';
+																if (window.confirm(message)) {
+																	eliminarCliente({
+																		variables: { id }
+																	})
+																}
+															} }
+														>
+															&times; Eliminar cliente
+														</button>
+													) }
+												</Mutation>
 												<Link to={`/cliente/editar/${cliente.id}`} className="btn btn-success d-block d-md-inline-block">
 													Editar cliente
 												</Link>
