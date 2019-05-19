8dd495b04199c6d80182d8f58b626c29ac1babec
Dídac García <didaquis@gmail.com>
2019-05-19 12:25:21 +0200

    Added state and validations to registry form

--- src/components/auth/Registro.js
+++ src/components/auth/Registro.js
@@ -1,7 +1,30 @@
 import React, { Component, Fragment } from 'react';
 
+const initialState = {
+	usuario: '',
+	password: '',
+	repetirPassword: ''
+}
+
+
 class Registro extends Component {
-	state = {}
+	state = {
+		...initialState
+	}
+
+	actualizarState = (e) => {
+		const { name, value } = e.target;
+
+		this.setState({
+			[name] : value
+		});
+	}
+
+	validarForm = () => {
+		const { usuario, password, repetirPassword } = this.state;
+		const noValido = !usuario || !password || !repetirPassword || password !== repetirPassword;
+		return noValido;
+	}
 
 	render() {
 		return (
@@ -12,34 +35,42 @@ class Registro extends Component {
 					<form className="col-md-8">
 						<div className="form-group">
 							<label>Usuario</label>
-							<input 
-								type="text" 
-								name="usuario" 
-								className="form-control" 
-								placeholder="Nombre Usuario" 
+							<input
+								onChange={this.actualizarState}
+								type="text"
+								name="usuario"
+								className="form-control"
+								placeholder="Nombre Usuario"
+								required
+								autoFocus
 							/>
 						</div>
 						<div className="form-group">
 							<label>Password</label>
-							<input 
-								type="password" 
-								name="password" 
-								className="form-control" 
+							<input
+								onChange={this.actualizarState}
+								type="password"
+								name="password"
+								className="form-control"
 								placeholder="Password"
+								required
 							/>
 						</div>
 						<div className="form-group">
 							<label>Repetir Password</label>
-							<input 
-								type="password" 
-								name="repetirPassword" 
-								className="form-control" 
-								placeholder="Repetir Password" 
+							<input
+								onChange={this.actualizarState}
+								type="password"
+								name="repetirPassword"
+								className="form-control"
+								placeholder="Repetir Password"
+								required
 							/>
 						</div>
 
-						<button 
-							type="submit" 
+						<button
+							disabled={ this.validarForm() }
+							type="submit"
 							className="btn btn-success float-right">
 								Crear Usuario
 						</button>
